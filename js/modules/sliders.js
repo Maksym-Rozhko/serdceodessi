dfb30aa46c1ef19143a9ea6469191de48805d4f4
Maksym <maksymRozhko7@gmail.com>
2022-04-05 14:54:46 +0300

    feat: add new sections our team, how help fund, media, styles, adaptive, init sliders

--- js/modules/sliders.js
+++ js/modules/sliders.js
@@ -22,6 +22,53 @@ const slidersInit = () => {
             }
         }
     });
+
+    const swiperTeam = new Swiper('.team .swiper', {
+      slidesPerView: 4,
+      spaceBetween: 10,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.team .swiper-button-next',
+        prevEl: '.team .swiper-button-prev',
+      },
+      breakpoints: {
+          320: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+          1480: {
+            slidesPerView: 4,
+          }
+      }
+  });
+
+  const swiperMedia = new Swiper('.media .swiper', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10
+        },
+        620: {
+          slidesPerView: 2,
+          spaceBetween: 15
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        }
+    }
+});
 };
 
 export default slidersInit;
